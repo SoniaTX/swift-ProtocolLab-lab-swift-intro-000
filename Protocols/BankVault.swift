1bde74e8bbab17427f0a639da7857025e3ec3363
SoniaTX <sonyaq@gmail.com>
2017-03-08 13:40:36 -0600

    Add completed protocol lab

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+import Foundation
+import QuartzCore
+
 
 class BankVault {
     
@@ -18,6 +21,33 @@ class BankVault {
         self.address = address
     }
     
+}
+
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
+
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        var rightPassword: Bool = false
+        
+        if password.isEmpty {
+            rightPassword = false
+        }
+        if password.count > 10 {
+            rightPassword = false
+        }
+        for (index, value) in password.enumerated() {
+            if index % 2 == 0 && value % 2 == 0 {
+               rightPassword = true
+            } else {
+               rightPassword = false
+            }
+        }
+        return rightPassword
+    }
+    
+ 
 }
 
 
